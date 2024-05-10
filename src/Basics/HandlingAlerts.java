de51921eceec56b788069987129ddaa8b2fecea3
Aravind <aravin12d@gmail.com>
2024-05-10 18:28:40 +0530

    this is from india

--- src/Basics/HandlingAlerts.java
+++ src/Basics/HandlingAlerts.java
@@ -1,12 +1,18 @@
 package Basics;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HandlingAlerts {
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException, IOException {
 		System.setProperty("Webdriver.chrome.driver",
 				"C:\\Users\\aravind.a05\\Downloads\\chromedriver-win64 (2)\\chromedriver-win64\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
@@ -20,6 +26,10 @@ public class HandlingAlerts {
 		driver.findElement(By.xpath("//input[@id='confirmbtn']")).click();
 		System.out.println(driver.switchTo().alert().getText());
 		driver.switchTo().alert().dismiss();
+		
+		//screenshot
+		File src =((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src, new File("D://sdfsdfsd"));
 		
 
 	}
